afe8dae8f830f27b945b0c08d0949a035206e5d1
Joseph <goddoft@gmail.com>
2017-09-27 12:03:42 +1000

    project outline (TODO.txt) + TODO.txt _ Seting up the environment for the automaton to live in

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 let width = window.innerWidth
 let height = window.innerHeight
 
-
 console.log('refreshed page')
+
+// page controlls
+function refresh() {
+  // TODO: refresh automaton
+  console.log('TODO: refresh automaton')
+}
+
+// TODO: firstGen
+
+
+
+// TODO: ruleTranstlator
+let ruleTranslator = rule=> {
+  // return binary array of rule
+}
