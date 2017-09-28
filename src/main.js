204a95e36528ffdf40f4ee5ecace0df28a74b7d6
Joseph <goddoft@gmail.com>
2017-09-28 19:46:58 +1000

    Automaton methods

--- src/main.js
+++ src/main.js
@@ -9,9 +9,26 @@ function refresh() {
   // TODO: refresh automaton
   console.log('TODO: refresh automaton')
 }
+let settings = {random: false, length: 100}
 
-// TODO: firstGen
-
+let firstGen = (settings)=>{
+  let r = settings.random
+  let l = settings.length
+  let a = []
+  if(r){
+    // populate firstGen randomly
+    for(i = 0; i<l; i++){
+      a.push(Math.floor(1&Math.random()*2))
+    }
+    return a
+  } else {
+    // populate firstGen with all cells empty except the middle cell
+    for(i = 0; i<l; i++) {
+      a.push(i===l>>>1?1:0)
+    }
+    return a
+  }
+}
 
 let ruleTranslator = r=> {
   let a = []
@@ -20,6 +37,10 @@ let ruleTranslator = r=> {
   }
   return a
 }
-// TODO: loopArr
+
+let loopArray = (arr, pos)=> {
+  let l = arr.length
+  return pos in arr?arr[pos]:(pos<l?arr[l+pos]:arr[pos%l])
+}
 
 // TODO: nextGen
