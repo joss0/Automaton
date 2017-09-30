ff1a29327c0c4705040f075b17b1fadf43ea8103
Joseph <goddoft@gmail.com>
2017-09-30 20:13:25 +1000

    A whole lot of fixes

--- src/main.js
+++ src/main.js
@@ -9,79 +9,76 @@ function refresh() {
   // TODO: refresh automaton
   console.log('TODO: refresh automaton')
 }
-let settings = {random: false, length: 100}
 
-let firstGen = (settings)=>{
-  let r = settings.random
-  let l = settings.length
-  let a = []
-  if(r){
+let firstGen = (settings)=> {
+  let a=[]
+  if(settings.isRandom){
     // populate firstGen randomly
-    for(i = 0; i<l; i++){
-      a.push(Math.floor(1&Math.random()*2))
+    for(i=0; i < settings.length; i++){
+      a.push(Math.floor(1&Math.isRandom()*2))
     }
     return a
   } else {
     // populate firstGen with all cells empty except the middle cell
-    for(i = 0; i<l; i++) {
-      a.push(i===l>>>1?1:0)
+    for(i=0; i < settings.length; i++) {
+      a.push(i === settings.length>>1?1:0)
     }
     return a
   }
 }
 
-let ruleTranslator = r=> {
+let ruleTranslator = (rule)=> {
   let a = []
   for(let i=0; i<8; i++) {
-    a.push(r >> i & 1)
+    a.push(rule>>7-i & 1)
   }
   return a
 }
 
-let loopArray = (arr, pos)=> {
-  let l = arr.length
-  return pos in arr?arr[pos]:(pos<l?arr[l+pos]:arr[pos%l])
+let loopArr = (len, pos)=> {
+  if(pos >= 0 && pos < len) {
+    return pos
+  } else if(pos < 0) {
+    while(pos < 0) {
+      pos+=len
+    }
+    return pos
+  } else {
+    return pos%len
+  }
 }
 
 let ancestors = (arr, pos)=> {
   let a = []
   for(let i=0; i<3; i++){
-    a.push(loopArray(arr, i+pos-1))
+    a.push(arr[loopArr(arr.length, i+pos-1)])
   }
   return a
 }
 
 let ruleChecker = (anc, rule)=> {
   let a = 0
-  if(anc[0]){
+  if(anc[0]) {
     a=a+4
   }if(anc[1]) {
     a=a+2
-  }if(anc[2]){
+  }if(anc[2]) {
     a=a+1
   }
   return rule[7-a]
 }
 
 let nextGen = (auto, rule)=> {
-  if(rule===undefined) {
-    auto = []
-    auto.push(firstGen({isRandom: false, length: 20}))
-  } else {
-    auto = auto[auto.length-1].map((x,i,arr)=> {
-      return ruleChecker(
-        ancestors(arr, i),
-        rule
-      )
-    })
-  }
+  auto = auto[auto.length-1].map((x,i,arr)=> {
+    return ruleChecker(ancestors(arr, i),rule)
+  })
   return auto
 }
 
 const Automaton = {
   firstGen: firstGen,
   ruleTranslator: ruleTranslator,
-  loopArray: loopArray,
+  loopArr: loopArr,
   ancestors: ancestors,
   ruleChecker: ruleChecker,
   nextGen: nextGen,
@@ -93,7 +90,6 @@ let automaton = (settings)=> {
   aut.ruleArr = aut.ruleTranslator(aut.settings.ruleNumber)
   aut.feild = [aut.firstGen(aut.settings)]
   aut.next = ()=> {
-    let feild = aut.feild
     aut.feild.push(aut.nextGen(aut.feild, aut.ruleArr))
   }
   return aut
